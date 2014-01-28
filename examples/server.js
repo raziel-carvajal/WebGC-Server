2778717b544aebe01c39b6f901c7baea48f00942
raziel <raziel.carvajal-gomez@inria.fr>
2014-01-28 19:27:30 +0100

    This version of the server deletes a infinity loop

--- examples/server.js
+++ examples/server.js
@@ -1,2 +1,6 @@
 var GossipPeerServer = require('../lib/peerjs-gossip-server.js').GossipPeerServer;
-var server = new GossipPeerServer({ port: process.argv[2], debug: true, startExpTimeout: process.argv[3] });
+var server = new GossipPeerServer({
+  port: process.argv[2], 
+  debug: true, 
+  firstViewSize: process.argv[3]
+});
