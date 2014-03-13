6a1d8f83ba293ce60f23541bd9db38ab9135ac41
raziel <raziel.carvajal-gomez@inria.fr>
2014-03-13 18:04:50 +0100

    The Grunt specification and some grunt-based plugins were added for improving the development of the project

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    linter: {
+      options: {
+        log: './logs/<%= pkg.name %>.log'
+      },
+      files: ['lib/peerjs-gossip-server.js']
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-linter');
+
+  // Default task(s).
+  grunt.registerTask('default', ['linter']);
+};
